541d059a4bd862a084bf2f650c4ee7f61614a4b9
uphy <yuhi.ishikura@gmail.com>
2021-08-31 13:51:18 +0900

    Extract reminder parser.

--- src/model/content.ts
+++ src/model/content.ts
@@ -1,9 +1,6 @@
 import { Reminder } from "./reminder";
 import { DateTime, DATE_TIME_FORMATTER, Time } from "./time";
 
-const reminderRegexp =
-  /^(?<prefix>\s*)\- \[(?<check>.)\]\s(?<title1>.*?)\(@(?<time>.+?)\)(?<title2>.*)$/;
-
 export class ReminderLine {
   constructor(
     public prefix: string,
@@ -24,8 +21,66 @@ export class ReminderEdit {
   public rawTime: string | null = null;
 }
 
+interface ReminderFormat {
+  parse(file: string, lineIndex: number, line: string): Reminder | null
+  modify(line: string, edit: ReminderEdit): string;
+}
+
+class DefaultReminderFormat implements ReminderFormat {
+
+  private static reminderRegexp = /^(?<prefix>\s*)\- \[(?<check>.)\]\s(?<title1>.*?)\(@(?<time>.+?)\)(?<title2>.*)$/;
+
+  parse(file: string, lineIndex: number, line: string): Reminder | null {
+    const parsed = this.parseReminderLine(line);
+    if (parsed === null) {
+      return null;
+    }
+    if (parsed.check === "x") {
+      return null;
+    }
+
+    const title = `${parsed.title1.trim()} ${parsed.title2.trim()}`.trim();
+    const parsedTime = DATE_TIME_FORMATTER.parse(parsed.time);
+    if (parsedTime !== null) {
+      return new Reminder(file, title, parsedTime, lineIndex);
+    }
+    return null;
+  }
+
+  modify(line: string, edit: ReminderEdit): string {
+    const r = this.parseReminderLine(line);
+    if (r === null) {
+      throw `not a reminder line: ${line}`;
+    }
+    if (edit.checked !== null) {
+      r.check = edit.checked ? "x" : " ";
+    }
+    if (edit.rawTime !== null) {
+      r.time = edit.rawTime;
+    } else if (edit.time !== null) {
+      r.time = DATE_TIME_FORMATTER.toString(edit.time);
+    }
+    return r.toLine();
+  }
+
+  private parseReminderLine(line: string): ReminderLine | null {
+    const result = DefaultReminderFormat.reminderRegexp.exec(line);
+    if (result === null) {
+      return null;
+    }
+    const prefix = result.groups.prefix;
+    const check = result.groups.check;
+    const title1 = result.groups.title1;
+    const time = result.groups.time;
+    const title2 = result.groups.title2;
+    return new ReminderLine(prefix, check, title1, time, title2);
+  }
+
+}
+
 export class Content {
   private lines: Array<string>;
+  private format: ReminderFormat = new DefaultReminderFormat();
 
   constructor(private file: string, content: string) {
     this.lines = content.split("\n");
@@ -38,18 +93,12 @@ export class Content {
   public getReminders(): Array<Reminder> {
     const reminders: Array<Reminder> = [];
     this.forEachLines((line, lineIndex) => {
-      const parsed = this.parseReminderLine(line);
+      const parsed = this.format.parse(this.file, lineIndex, line);
       if (parsed === null) {
         return;
       }
-      if (parsed.check === "x") {
-        return;
-      }
-
-      const title = `${parsed.title1.trim()} ${parsed.title2.trim()}`.trim();
-      const parsedTime = DATE_TIME_FORMATTER.parse(parsed.time);
-      if (parsedTime !== null) {
-        reminders.push(new Reminder(this.file, title, parsedTime, lineIndex));
+      if (parsed !== null) {
+        reminders.push(parsed);
       }
     });
     return reminders;
@@ -77,19 +126,7 @@ export class Content {
     edit: ReminderEdit
   ) {
     const line = this.getLine(index);
-    const r = this.parseReminderLine(line);
-    if (r === null) {
-      throw `not a reminder line: ${line}`;
-    }
-    if (edit.checked !== null) {
-      r.check = edit.checked ? "x" : " ";
-    }
-    if (edit.rawTime !== null) {
-      r.time = edit.rawTime;
-    } else if (edit.time !== null) {
-      r.time = DATE_TIME_FORMATTER.toString(edit.time);
-    }
-    const newLine = r.toLine();
+    const newLine = this.format.modify(line, edit);
     this.lines[index] = newLine;
     console.info(
       "Modify reminder line: file=%s, index=%d, oldLine=%s, newLine=%s",
@@ -102,19 +139,6 @@ export class Content {
 
   private forEachLines(consumer: (line: string, lineIndex: number) => void) {
     this.lines.forEach(consumer);
-  }
-
-  private parseReminderLine(line: string): ReminderLine | null {
-    const result = reminderRegexp.exec(line);
-    if (result === null) {
-      return null;
-    }
-    const prefix = result.groups.prefix;
-    const check = result.groups.check;
-    const title1 = result.groups.title1;
-    const time = result.groups.time;
-    const title2 = result.groups.title2;
-    return new ReminderLine(prefix, check, title1, time, title2);
   }
 
   public getContent(): string {
