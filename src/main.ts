9210df08d30f1036d3a1d19cb32a0e180379a7bd
uphy <yuhi.ishikura@gmail.com>
2025-01-05 07:16:44 +0900

    refactor: extract notification-worker

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,14 @@ import { SETTINGS } from 'plugin/settings';
 import { registerCommands } from 'plugin/commands';
 import { ReminderPluginUI } from 'plugin/ui';
 import { ReminderPluginFileSystem } from 'plugin/filesystem';
+import { NotificationWorker } from 'plugin/notification-worker';
 
 export default class ReminderPlugin extends Plugin {
   _pluginDataIO: PluginDataIO;
   private _ui: ReminderPluginUI;
   private _reminders: Reminders;
   private _fileSystem: ReminderPluginFileSystem;
-
+  private _notificationWorker: NotificationWorker;
   constructor(app: App, manifest: PluginManifest) {
     super(app, manifest);
     this._reminders = new Reminders(() => {
@@ -31,6 +32,7 @@ export default class ReminderPlugin extends Plugin {
     this._fileSystem = new ReminderPluginFileSystem(app.vault, this.reminders, () => {
       this.ui.reload(true);
     });
+    this._notificationWorker = new NotificationWorker(this);
   }
 
   override async onload() {
@@ -42,78 +44,8 @@ export default class ReminderPlugin extends Plugin {
         monkeyPatchConsole(this);
       }
       this.fileSystem.onload(this);
-      this.startPeriodicTask();
+      this._notificationWorker.startPeriodicTask();
     });
-  }
-
-  private startPeriodicTask() {
-    let intervalTaskRunning = true;
-    // Force the view to refresh as soon as possible.
-    this.periodicTask().finally(() => {
-      intervalTaskRunning = false;
-    });
-
-    // Set up the recurring check for reminders.
-    this.registerInterval(
-      window.setInterval(() => {
-        if (intervalTaskRunning) {
-          console.log('Skip reminder interval task because task is already running.');
-          return;
-        }
-        intervalTaskRunning = true;
-        this.periodicTask().finally(() => {
-          intervalTaskRunning = false;
-        });
-      }, SETTINGS.reminderCheckIntervalSec.value * 1000),
-    );
-  }
-
-  private async periodicTask(): Promise<void> {
-    this.ui.reload(false);
-
-    if (!this.pluginDataIO.scanned.value) {
-      this.fileSystem.reloadRemindersInAllFiles().then(() => {
-        this.pluginDataIO.scanned.value = true;
-        this.pluginDataIO.save();
-      });
-    }
-
-    this.pluginDataIO.save(false);
-
-    if (this.ui.isEditing()) {
-      return;
-    }
-    const expired = this.reminders.getExpiredReminders(SETTINGS.reminderTime.value);
-
-    let previousReminder: Reminder | undefined = undefined;
-    for (const reminder of expired) {
-      if (this.app.workspace.layoutReady) {
-        if (reminder.muteNotification) {
-          // We don't want to set `previousReminder` in this case as the current
-          // reminder won't be shown.
-          continue;
-        }
-        if (previousReminder) {
-          while (previousReminder.beingDisplayed) {
-            // Displaying too many reminders at once can cause crashes on
-            // mobile. We use `beingDisplayed` to wait for the current modal to
-            // be dismissed before displaying the next.
-            await this.sleep(100);
-          }
-        }
-        this.ui.showReminder(reminder);
-        previousReminder = reminder;
-      }
-    }
-  }
-
-  /* An asynchronous sleep function. To use it you must `await` as it hands
-   * off control to other portions of the JS control loop whilst waiting.
-   *
-   * @param milliseconds - The number of milliseconds to wait before resuming.
-   */
-  private async sleep(milliseconds: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, milliseconds));
   }
 
   override onunload(): void {
